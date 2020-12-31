6c9cbb569b3ce8bd8a1681bdb123827da4be35f5
Gazi Salahuddin <71849295+Salahuddin-Gazi@users.noreply.github.com>
2021-01-01 04:11:56 +0600

    1 Jan 2021

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+import { ButtonContainer } from "./Button";
+import styled from "styled-components";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        {/* Using it for web? Copy this link in your website: <div>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+        <Link to="/">
+          <img src={logo} alt="store" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
+            my cart
+          </ButtonContainer>
+        </Link>
+      </NavWrapper>
+    );
+  }
+}
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+  }
+`;
